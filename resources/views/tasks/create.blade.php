e246998d4539df4950b7864bbf61cc19d888d03d
nikochan <nikochan@gmail.com>
2018-06-01 06:33:18 +0000

    bootstrap designdesign

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,23 @@
 @section('content')
 
     <h1>TASKS NEW PAGE</h1>
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-offset3-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'STATUS:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'TASK:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+            
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
 @endsection
